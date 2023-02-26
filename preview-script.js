e0fb71cecdd399f26b0df3531c04528158f23529
Noname <120730486+SheetGuy@users.noreply.github.com>
2023-02-26 09:40:43 -0300

    Delete preview-script.js

--- preview-script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.preview-content').innerHTML = localStorage.getItem('valueHtml');
-document.querySelector('.style-content').innerHTML = localStorage.getItem('valueCss');
-eval(localStorage.getItem('valueJs'));
